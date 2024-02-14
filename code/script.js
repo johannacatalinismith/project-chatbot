5a0e62859f05fbb26b0fe45a4f160d9303d28a17
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-02-14 20:36:16 +0100

    Add show message from user, add size options, price, checkout and order confirmation.

--- code/script.js
+++ code/script.js
@@ -7,6 +7,8 @@ const sendBtn = document.getElementsByClassName("send-btn");
 
 // Here you declare variables to work with👇
 // Johannas has done some changes here; adding more varibles to declare.
+
+//Global variables
 let userName = "";
 let foodChoice = "";
 let subTypeFood = "";
@@ -45,10 +47,11 @@ const showMessage = (message, sender) => {
         </div>
       </section>`;
   }
+  //This is to make the page scroll.
+  chat.scrollTop = chat.scrollHeight;
 };
 
 //This is how to call the function (The bot says hello and ask for your name)
-
 greetUser();
 
 //This is the event listener (the user tell the bot their name)
@@ -64,7 +67,7 @@ nameForm.addEventListener("submit", (e) => {
   showMessage(`${userName}`, "user");
   //nameinput value "" remove the text from the input field after user has typed and send msg.
   nameInput.value = "";
-  setTimeout(chooseFood, 2000);
+  setTimeout(chooseFood, 1500);
 });
 
 //This function asks what type of food the user wants to eat
@@ -83,6 +86,7 @@ function addButtons() {
     <button class="send-btn" id="salad">Salad</button>`;
   document.querySelector("#pizza").addEventListener("click", () => {
     foodChoice = "pizza";
+    showMessage(foodChoice, "user");
     chosePizzaType();
   });
 }
@@ -97,9 +101,12 @@ const chosePizzaType = () => {
     <option value="Neapolitan">Neapolitan</option>
     <option value="Sicilian">Sicilian</option>
   </select>`;
-  const select = document.getElementById("select");
   document.querySelector("select").addEventListener("change", (e) => {
     subTypeFood = e.target.value;
+    //This is a function that show the message from the user.
+    showMessage(subTypeFood, "user");
+    //This is where I call the function.
+    pizzaTypeConfirm();
   });
 };
 
@@ -109,21 +116,57 @@ const pizzaTypeConfirm = () => {
     <button class="send-btn" id="Yes">Yes</button>
     <button class="send-btn" id="No">No</button>`;
   document.querySelector("#Yes").addEventListener("click", () => {
-    foodSize();
+    //How to show message from the user
+    showMessage("Yes", "user");
+    showSizeOptions();
   });
   document.querySelector("#No").addEventListener("click", () => {
-    chosePizzaType();
+    showMessage("No", "user");
+    showSizeOptions();
   });
 };
 
-foodSize = () => {
+const showSizeOptions = () => {
   showMessage(`What size do you want?`, "bot");
   inputWrapper.innerHTML = `
     <button class="send-btn" id="Small">Small</button>
     <button class="send-btn" id="Big">Big</button>`;
-  document.querySelector("#Small").addEventListener("click", () => {});
+  document.querySelector("#Small").addEventListener("click", () => {
+    foodSize = "small";
+    price = 10;
+    checkOut();
+  });
 
-  document.querySelector("#Big").addEventListener("click", () => {});
+  document.querySelector("#Big").addEventListener("click", () => {
+    foodSize = "big";
+    price = 15;
+    checkOut();
+  });
+};
+
+const checkOut = () => {
+  showMessage(
+    `One ${foodSize} ${subTypeFood} will be prepared for you. That will be € ${price}. 
+  Are you sure you want to order this?`,
+    "bot"
+  );
+  inputWrapper.innerHTML = `
+    <button class="send-btn" id="Yes">Yes</button>
+    <button class="send-btn" id="No">No</button>`;
+  document.querySelector("#Yes").addEventListener("click", () => {
+    showMessage("Yes", "user");
+    orderConfirmation();
+  });
+  document.querySelector("#No").addEventListener("click", () => {
+    showMessage("No", "user");
+    orderConfirmation();
+  });
+};
+
+const orderConfirmation = () => {
+  showMessage(`Thank you! See you again!`, "bot");
+  //This inputwrapper "" get rid of the buttons.
+  inputWrapper.innerHTML = "";
 };
 
 /*
@@ -143,7 +186,7 @@ const chosePizzaType = () => {
 
   // This little thing makes the chat scroll to the last message when there are too many to
   // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
+  
 };
 
 
